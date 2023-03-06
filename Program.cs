a3b22cf2f26a2ea19ccc1c81501f6ebab5297b57
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-03-06 23:44:22 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quanityRepeat;
+            string userMessage;
+
+            Console.Write($"Введите текст - ");
+            userMessage = (Console.ReadLine());
+
+            Console.Write($"Введите количество повторов - ");
+            quanityRepeat = Convert.ToInt32(Console.ReadLine());
+
+            while (quanityRepeat-- > 0 ) 
+            {
+                Console.WriteLine( userMessage );     
+            } 
+        }
+    }
+}
